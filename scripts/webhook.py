0a8b07ff55010261f6e627490668687ee9e48a56
Autistic Eevee <autisticeevee@gmail.com>
2024-03-09 22:52:21 +0530

    simple discord webhook function

---
+++ scripts/webhook.py
@@ -0,0 +1,20 @@
+from youtube import *
+
+load_dotenv()
+webhook_urls = os.getenv('TESTHOOK_URLS').split('|')
+
+async def send_hook(message):
+    urls = webhook_urls
+    async with httpx.AsyncClient() as session:
+        for url in webhook_urls:
+            try:
+                payload = {'content': message}
+                print(message)
+                response = await session.post(url,  json=payload, timeout=10)
+                if response.status_code == 204:
+                    print(f'Posted to: {url}')
+                else:
+                    print(f'Failed to post to: {url}')
+                await asyncio.sleep(3)
+            except Exception as e:
+                print(f'Critical error sending to webhook: {url} : {str(e)}')
