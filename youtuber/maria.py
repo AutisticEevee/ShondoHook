c06ea25f2ee8ef52c59c0c29be2895cf33d64183
Autistic Eevee <autisticeevee@gmail.com>
2024-03-10 16:57:33 +0530

    idk how to stop

--- youtuber/maria.py
+++ youtuber/maria.py
@@ -1,10 +1,24 @@
-from scripts.youtube import get_random_ass_video_link
+from scripts.youtube import get_random_ass_video_link, get_latest_video
 from dotenv import load_dotenv
 import asyncio
 import os
 
 load_dotenv()
 maria = os.getenv('MARIA')
+latest_video_id = None
 
 async def random_maria():
     return await get_random_ass_video_link(maria)
+
+def is_latest_video(video_id):
+    if video_id != latest_video_id:
+        return True
+    else:
+        return False
+
+async def latest_maria():
+    video_title, video_url, video_id, date_published = await get_latest_video(maria)
+    if is_latest_video(video_id):
+        latest_video_id = video_id
+        return video_title, video_url, date_published
+   
--- youtuber/shungite.py
+++ youtuber/shungite.py
@@ -1,10 +1,27 @@
-from scripts.youtube import get_random_ass_video_link
+from scripts.youtube import get_random_ass_video_link, get_latest_video
 from dotenv import load_dotenv
 import os
 import asyncio
 
 load_dotenv()
 shungite = os.getenv('SHONDOPILL')
+latest_video_id = None
 
 async def random_shungite():
     return await get_random_ass_video_link(shungite)
+
+def is_latest_video(video_id):
+    if video_id != latest_video_id:
+        return True
+    else:
+        return False
+
+async def latest_shungite():
+    video_title, video_url, video_id, date_published = await get_latest_video(shungite)
+    if is_latest_video(video_id):
+        latest_video_id = video_id
+        return video_title, video_url, date_published
+    
+
+    
+
