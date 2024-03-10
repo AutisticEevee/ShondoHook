6cf3a1b72457e4f6e937fdf0032b737a3774ad25
Autistic Eevee <autisticeevee@gmail.com>
2024-03-10 13:21:35 +0530

    shungite and maria

--- youtuber/maria.py
+++ youtuber/maria.py
@@ -1,1 +1,10 @@
-from scripts.youtube import *
+from scripts.youtube import get_random_ass_video_link
+from dotenv import load_dotenv
+import asyncio
+import os
+
+load_dotenv()
+maria = os.getenv('MARIA')
+
+async def random_maria():
+    return await get_random_ass_video_link(maria)
--- youtuber/shungite.py
+++ youtuber/shungite.py
@@ -1,1 +1,10 @@
-from scripts.youtube import *
+from scripts.youtube import get_random_ass_video_link
+from dotenv import load_dotenv
+import os
+import asyncio
+
+load_dotenv()
+shungite = os.getenv('SHONDOPILL')
+
+async def random_shungite():
+    return await get_random_ass_video_link(shungite)
