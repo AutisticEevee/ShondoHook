75833c9ae4d077b35e58574df9e331130c7bac7a
Autistic Eevee <autisticeevee@gmail.com>
2024-03-10 23:12:05 +0530

    sigh...

--- youtuber/maria.py
+++ youtuber/maria.py
@@ -1,10 +1,13 @@
-from scripts.youtube import get_random_ass_video_link, get_latest_video
+from scripts.youtube import get_random_ass_video_link, get_latest_video, get_channel_profile_picture, get_channel_name, url_to_title
+from scripts.webhook import send_hook, set_webhook_avatar
 from dotenv import load_dotenv
 import asyncio
 import os
 
 load_dotenv()
 maria = os.getenv('MARIA')
+maria_webhook_urls = os.getenv('MARIA_WEBHOOK_URLS').split('|')
+artlover_username = asyncio.run(get_channel_name(maria))
 latest_video_id = None
 
 async def random_maria():
@@ -21,4 +24,24 @@ async def latest_maria():
     if is_latest_video(video_id):
         latest_video_id = video_id
         return video_title, video_url, date_published
-   
+    
+async def maria_hook_avatar():
+    image_url = await get_channel_profile_picture(maria)
+    await set_webhook_avatar(image_url, maria_webhook_urls)
+    
+async def announce_maria_random_upload():
+    video_url = await random_maria()
+    video_title = await url_to_title(video_url)
+    announcement = f'''
+    Hey hey hey, my lovely art enthusiasts! ٩(๑❛ᴗ❛๑)۶ It's your girl, ArtLoverMaria, back at it again with a blast from the past! 💥 Remember that one time when we absolutely crushed it with our cover of **{video_title}**? 🎵 Oh boy, oh boy, that was a wild ride! 🎢 I was just rewatching it and I couldn't help but think, "Dang, we really poured our hearts and souls into that one!" (๑•̀ㅂ•́)و✧ The way we- *music starts playing* and then when we- *music gets louder* absolutely mind-blowing! 🤯 If you haven't seen it yet, you're totally missing out! 😱 Go check it out right now on our YouTube channel and prepare to be blown away! 🌪️ Don't forget to leave a like and a comment, because you know how much we love hearing from you! (≧◡≦) ♡ Anyways, that's all from me for now! 😘 Keep being the awesome, art-loving community that you are! 🌈 Peace out, my lovelies! 🙌💕 Your quirky artist, ArtLoverMaria, signing off! (⌒‿⌒)
+    {video_url}
+    '''
+    await send_hook(announcement, artlover_username, maria_webhook_urls)
+        
+async def announce_maria_new_upload():
+    video_title, video_url, date_published = await latest_maria()
+    announcement = f'''
+    Heyy everyone~! (๑˃̵ᴗ˂̵)و It's your favorite quirky artist, ArtLoverMaria, coming at you with some super duper exciting news! ✨ Are you ready? Drumroll please... 🥁🥁🥁 *Bum bum bum bum* Our brand new cover is finally here!!! 🎉🎶 It's called **{video_title}** and it's an absolute banger! (ﾉ´ヮ`)ﾉ*: ･ﾟ We poured our hearts and souls into this one, so I really hope you'll all love it as much as we- *music starts playing loudly* do! ❤️ Make sure to smash that like button and- *music drowns out speech* leave us a comment letting us know what you think! 😄 You can check it out right now on- *music intensifies* YouTube channel! 🎥 Thanks so much for all your support, you guys are the absolute best! (´｡• ω •｡`) ♡ Lots of love and hugs from your favorite artist, ArtLoverMaria! 🤗💕
+    {video_url}
+    '''
+    await send_hook(announcement, artlover_username, maria_webhook_urls)
--- youtuber/shungite.py
+++ youtuber/shungite.py
@@ -1,11 +1,14 @@
-from scripts.youtube import get_random_ass_video_link, get_latest_video
+from scripts.youtube import get_random_ass_video_link, get_latest_video, get_channel_profile_picture, get_channel_name, url_to_title
+from scripts.webhook import send_hook, set_webhook_avatar
 from dotenv import load_dotenv
 import os
 import asyncio
 
 load_dotenv()
 shungite = os.getenv('SHONDOPILL')
+shungite_webhook_urls = os.getenv('SHONDO_WEBHOOK_URLS').split('|')
 latest_video_id = None
+inferno_username = asyncio.run(get_channel_name(shungite))
 
 async def random_shungite():
     return await get_random_ass_video_link(shungite)
@@ -21,7 +24,42 @@ async def latest_shungite():
     if is_latest_video(video_id):
         latest_video_id = video_id
         return video_title, video_url, date_published
+    else:
+        return video_title, video_url, date_published
     
-
+async def shungo_hook_avatar():
+    image_url = await get_channel_profile_picture(shungite)
+    await set_webhook_avatar(image_url, shungite_webhook_urls)
     
+async def announce_shungo_random_upload():
+    video_url = await random_shungite()
+    video_title = await url_to_title(video_url)
+    announcement = f'''
+Nyaa, nyaa, nyaa! (~⁰▿⁰)~ Listen up, my adorable onii-chans and onee-chans! Your favorite bratty imouto cat girl Shondo here, and I've got something super special to share with you all! ٩(^ᴗ^)۶ I was just browsing through my stream clips channel and stumbled upon an absolute gem that I totally forgot about! 😹💎
 
+Remember that one time when I absolutely slayed in **{video_title}**? 🤔😏 Of course you do! How could anyone forget the legendary antics of the cutest imouto to ever grace the internet? 😼💜 Well, I've decided to bless your timeline with this incredible clip once again, because I'm just that generous! 😇🎀
+
+Now, I know some of you might be thinking, "But Shondo, we've already seen this clip before!" 🙄 Well, too bad! As your imouto overlord, I declare that you shall watch it again and shower me with even more love and adoration than before! 😈💕 Besides, who could ever get tired of my kawaii face and charming personality? 😽✨
+
+So, what are you waiting for, my loyal subjects? 🤔 Go forth and bask in the glory of **{video_title}** once more! 🌟 And while you're at it, don't forget to leave a like and a comment telling me how much you missed your favorite imouto! 😿💌 If this clip doesn't get all the love it deserves, I might just throw a tantrum and cause some mischief in the forest! 🌿😼
+
+Alright, that's all from your favorite purple-haired troublemaker for meow! \\(^ω^)/ Remember to stay sugoi, keep being awesome, and most importantly... never forget that Shondo wuvs you with all her heart! Nyaa~! 🐾😘💜 
+    {video_url}
+    '''
+    await send_hook(announcement, inferno_username, shungite_webhook_urls)
+        
+async def announce_shungo_new_upload():
+    video_title, video_url, date_published = await latest_shungite()
+    announcement = f'''
+Nyaa, nyaa, nyaa! (~⁰▿⁰)~ Attention all my lovely onii-chans and onee-chans! Your favorite imouto cat girl Shondo here with some super duper exciting news! ٩(^ᴗ^)۶ I've got a brand new stream clip that's so amazing, it'll make your whiskers tingle with joy! 😸💜
+
+Introducing... *drumroll please* 🥁🥁🥁 **{video_title}**! 🎉😻 This clip is so kawaii and sugoi, you won't believe your eyes! Your bratty little imouto has really outdone herself this time, if I do say so myself. 😏✨ 
+
+Now, I know what you're thinking. "But Shondo, we're so busy with our boring adult lives! How will we ever find the time to watch your incredible stream clip?" 😿 Well, fear not my dear followers! As your doting imouto, I've made it my mission to bring a little bit of chaos and fun into your dreary existence! 😼💕
+
+So drop everything you're doing right meow and watch **{video_title}** or else... I'll be a very sad and lonely kitty all alone in this big scary forest! 🍂🥺 And you wouldn't want to make your precious imouto cry, would you? 🥺💔 I thought so!
+
+Alright, that's all from your favorite purple-haired troublemaker for now! \\(^ω^)/ Don't forget to smash that like button and leave a comment telling me how much you adore me! 😽💜 Until next time, stay sugoi and remember... Shondo wuvs you! Nyaa~! 🐾😘
+    {video_url}
+    '''
+    await send_hook(announcement, inferno_username, shungite_webhook_urls)
